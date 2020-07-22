a6803392ab4d81b63837530021c75180f54d7f89
Ayoub Ennassiri <ayo.enn@gmail.com>
2020-07-22 18:49:50 +0200

    Adding KMeanHL clustering method

--- src/cluster.py
+++ src/cluster.py
@@ -1,17 +1,33 @@
+from itertools import groupby
+from operator import itemgetter
+
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_samples
 
 
-class KmeansBase(KMeans):
-    def __init__(self, max_n_clusters=10, n_init=10, **kwargs):
-        self.max_n_clusters = max_n_clusters
+class KMeansBase(KMeans):
+    def __init__(self, max_n_clusters=None, n_init=10, **kwargs):
+        """
+        KMeans Base Clustering method
+        This kmeans algorithm doesn't require a
+        :param max_n_clusters:
+        :type max_n_clusters:
+        :param n_init:
+        :type n_init:
+        :param kwargs:
+        :type kwargs:
+        """
+        self.max_n_clusters = None
         self.n_init = n_init
         self.quality = None
         self.silhouette = None
         super().__init__(n_init=n_init, **kwargs)
 
     def fit(self, corr, y=None, sample_weight=None):
+        if self.max_n_clusters is None:
+            self.max_n_clusters = corr.shape[1] - 1
+
         # Transform the correlation matrix to X
         X = ((1 - np.nan_to_num(corr)) / 2) ** 0.5
         # Init the best figures
@@ -22,7 +38,7 @@ class KmeansBase(KMeans):
         for _ in range(self.n_init):
             for i in range(2, self.max_n_clusters + 1):
                 self.n_clusters = i
-                kmeans = super().fit(X)
+                kmeans = super().fit(X, y=y, sample_weight=sample_weight)
                 silhouette = silhouette_samples(X, self.labels_)
                 quality = silhouette.mean() / silhouette.std()
                 if best_silhouette.size > 0 or quality > best_quality:
@@ -30,6 +46,7 @@ class KmeansBase(KMeans):
                     best_kmeans = kmeans
                     best_quality = quality
 
+        # We keep only the best kmeans data
         self.cluster_centers_ = best_kmeans.cluster_centers_
         self.labels_ = best_kmeans.labels_
         self.inertia_ = best_kmeans.inertia_
@@ -37,3 +54,28 @@ class KmeansBase(KMeans):
         self.quality = best_quality
         self.silhouette = best_silhouette
         return self
+
+
+class KmeansHL(KMeansBase):
+    def eval_scores(self):
+        clusters = groupby(
+            sorted(zip(self.labels_, self.silhouette)),
+            itemgetter(0)
+        )
+
+        for key, data in clusters:
+            silhouettes = tuple(data)
+            yield key, np.mean(silhouettes) / np.std(silhouettes)
+
+    def fit(self, corr, y=None, sample_weight=None):
+        super().fit(corr=corr, y=y, sample_weight=sample_weight)
+
+        clusters_scores = tuple(self.eval_scores())
+        clusters_scores_avg = np.mean(np.nan_to_num(clusters_scores))
+
+        cluster_redo = (
+            key for key, score in self.eval_scores()
+            if score < clusters_scores_avg
+        )
+
+        
--- src/test_clustering.py
+++ src/test_clustering.py
@@ -1,8 +1,9 @@
 import unittest
 
+
 import numpy as np
 import numpy.testing as npt
-from src.cluster import KmeansBase
+from src.cluster import KMeansBase, KmeansHL
 
 
 class KmeansBaseTestCase(unittest.TestCase):
@@ -17,7 +18,31 @@ class KmeansBaseTestCase(unittest.TestCase):
 
             ]
         )
-        kmeans = KmeansBase(max_n_clusters=4, random_state=0).fit(corr)
+        kmeans = KMeansBase(max_n_clusters=4, random_state=0).fit(corr)
+
+        # Assert the best quality calculation
+        npt.assert_almost_equal(kmeans.quality, 0.8164691740299027)
+
+        # Assert that the optimal number of clusters is 4
+        self.assertEqual(len(set(kmeans.labels_)), 4)
+        # Assert that the 1 and 2 belong to the same cluster as
+        # they are both correlated
+        self.assertEqual(kmeans.labels_[0], kmeans.labels_[1])
+
+
+class KmeansHLTestCase(unittest.TestCase):
+    def test_clustering(self):
+        corr = np.array(
+            [
+                [1, 0.9, -0.4, 0, 0],
+                [0.9, 1, -0.3, 0.1, 0],
+                [-0.4, -0.3, 1, -0.1, 0],
+                [0, 0.1, -0.1, 1, 0],
+                [0, 0, 0, 0, 1],
+
+            ]
+        )
+        kmeans = KmeansHL(max_n_clusters=4, random_state=0).fit(corr)
 
         # Assert the best quality calculation
         npt.assert_almost_equal(kmeans.quality, 0.8164691740299027)
